0cd56bc8fd294ee0c6cb516cea23a431a5bb261d
Yogendra Singh <yogendra.singhh@gmail.com>
2024-04-17 15:40:22 +0530

    ol development server data commit

--- admin_iiti/public/js/leave_application_custom.js
+++ admin_iiti/public/js/leave_application_custom.js
@@ -1,3 +1,22 @@
 frappe.ui.form.on("Leave Application", {
-	
+	setup: function(frm) {
+		frm.set_query("leave_approver", function() {
+			return {
+				query: "admin_iiti.overrides.get_approvers",
+				filters: {
+					'enabled': 1
+				}
+			};
+		});
+
+		frm.set_query("employee", erpnext.queries.employee);
+	},
+	leave_approver : function(frm) {
+		if (frm.doc.leave_approver) {			
+			frappe.db.get_value('User', {'name': frm.doc.leave_approver}, 'full_name', (d) =>{
+				frm.set_value("leave_approver_name",d.full_name);
+			})
+			
+		}
+    },
 });
