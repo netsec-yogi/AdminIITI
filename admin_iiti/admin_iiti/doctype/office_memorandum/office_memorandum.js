71574e141ca6ce6f8e81911841199d94025c61a5
princy awadhiya <princy@iiti.ac.in>
2025-02-21 12:26:40 +0530

    error sort in js file

--- admin_iiti/admin_iiti/doctype/office_memorandum/office_memorandum.js
+++ admin_iiti/admin_iiti/doctype/office_memorandum/office_memorandum.js
@@ -5,7 +5,6 @@ frappe.ui.form.on('Office Memorandum', {
 	// refresh: function(frm) {
 
 	// }
-<<<<<<< HEAD
 	notesheet_name:function(frm){
 		frm.set_query("document_name", function() {
 			return {
@@ -16,40 +15,4 @@ frappe.ui.form.on('Office Memorandum', {
 			};
 		});
 	}
-=======
-	
-	notesheet_name: function(frm) {
-        if (frm.doc.notesheet_name) {
-
-             // Set a dynamic query for document_name field
-             frm.set_query("document_name", function() {
-                return {
-                    doctype: frm.doc.notesheet_name, // Dynamically fetch from selected Doctype
-                    //filters: {'status':'Approved'},
-                    fields: ["name", "employee_name", "department"], // Fetch only required fields
-                    limit_page_length: 20 // Limit the number of results
-                };
-            });
-            // frappe.call({
-            //     method: 'frappe.client.get_list',
-            //     args: {
-            //         doctype: frm.doc.notesheet_name, // Get data from selected Doctype
-            //         fields: ['name', 'employee_name', 'department', 'department'], // Fetch only required fields
-            //         // limit_page_length: 10 // Limit number of results
-            //     },
-            //     callback: function(response) {
-			// 		console.log("mes",response.message);
-					
-            //         if (response.message) {
-            //             let options = response.message.map(row => 
-			// 				`${row.name} ( ${row.employee_name} ) ( ${row.department})`
-			// 			).join('\n');
-            //             frm.set_df_property('document_name', 'options', options);
-            //             frm.refresh_field('document_name');
-            //         }
-            //     }
-            // });
-        }
-    }
->>>>>>> 7b4d17635882a16b24822776cd25dafb7a4da77b
 });
